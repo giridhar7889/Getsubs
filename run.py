40dea2967943e20d88c33be1f7a123c33e21daf6
giridhar7889 <giridhar7889@gmail.com>
2022-04-25 19:06:31 +0530

    finsihed testing GetSubs/get_subs.py->download()

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.insert(0, "src/")
+
+
+from get_sub import GetSub
+
+
+def main(args):
+    if len(args) != 2:
+        sys.exit("usage: 'Give video file as input' 'specify language'")
+
+    gs = GetSub(3, 30, 300)
+    gs.download(args[0], args[1])
+
+
+if __name__ == "__main__":
+
+    main(sys.argv[1:])
--- subs_src/get_subs.py
+++ subs_src/get_subs.py
@@ -6,3 +6,33 @@ import numpy as np
 import srt
 import subprocess
 import pandas as pd
+from utilis import mkdir, basename_without_ext
+
+
+class GetSub:
+    def download(self, video_file_path, language):
+        # get the directory name from the video file path
+        file_dir = os.path.dirname(video_file_path)
+        temp_dir = "temp/"
+        # create a directory named temp and file_dir
+        mkdir(temp_dir)
+        mkdir(file_dir)
+
+        command1 = (
+            "python OpenSubtitlesDownload.py --cli --auto {} --output {} --lang {}"
+        )
+        command1_list = command1.format(video_file_path, temp_dir, language).split(" ")
+        subprocess.call(command1_list)
+
+        original_name = basename_without_ext(video_file_path)
+        srt_path = os.path.join(temp_dir, original_name + ".srt")
+
+        # save original file as 'filename_unsynced.srt'
+        out_path_unsynced = os.path.join(file_dir, original_name + "_unsynced.srt")
+        command2 = "srt fixed-timeshift --input {} --output {} --seconds 0"
+        command2_list = command2.format(srt_path, out_path_unsynced).split(" ")
+        subprocess.call(command2_list)
+
+        print("downloaded subs:", srt_path)
+
+        self.align(video_file_path, srt_path, file_dir, original_name)
